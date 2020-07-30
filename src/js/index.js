073179fbe9c5d406e5ba05e88e10e445f0a920a5
Julciik <jul.kolinko@gmail.com>
2020-07-30 19:05:39 +0200

    edit header-content and add nav mob res

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,13 @@
-console.log("hello world!");
-console.log("Testing!");
+const search = document.querySelector("[data-search-btn]");
+const searchInput = document.querySelector("[data-search-input]");
+const hamburger = document.querySelector("[data-hamburger]");
+const navMenu = document.querySelector("[data-menu]");
+
+search.addEventListener("click", () => {
+  searchInput.classList.toggle("search__active");
+});
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("hamburger__active");
+  navMenu.classList.toggle("menu__active");
+});
